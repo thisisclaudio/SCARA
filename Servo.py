6028e01e6b7add671c80ed632c3491dc72dfa9fa
nicoh <nicoheubi@hotmail.ch>
2026-05-15 16:04:11 +0200

    servo.py half way there, sepper unittest und servo unittest name ändern amk

--- Servo.py
+++ Servo.py
@@ -1,4 +1,4 @@
-class Motor:
+class Servo_Motor:
     def __init__(self, id, offset, packet_handler):
         self.id = id
         self.offset = offset
@@ -18,23 +18,38 @@ class Motor:
 
     def get_position_raw(self):
         position, _, _, _ = self.packet_handler.ReadPosSpeed(self.id)
-        return position - 4096 + self.offset 
+        if self.id == 1: 
+            return position - 4096 + self.offset 
+        else:
+            return position - 1024 + self.offset ## evtl anpassen siehe 0 pos
     
     def get_position(self):
         position_raw = self.get_position_raw()
-        return -position_raw * 2 * 3.141592653589793 / 4096
-    
+        if self.id == 1:
+            return -position_raw * 2 * 3.141592653589793 / 4096
+        else:
+            return position_raw * 2 * 3.141592653589793 / 1024
+
     def get_speed(self):
         _, speed, _, _ = self.packet_handler.ReadPosSpeed(self.id)
         return speed
     
     def set_position(self, position, speed=1000):
-        position_raw = int(-position * 4096 / (2 * 3.141592653589793) + 4096 - self.offset)
+        if self.id == 1:
+            position_raw = int(-position * 4096 / (2 * 3.141592653589793) + 4096 - self.offset)
+        else:
+            position_raw = int(position * 1024 / (2 * 3.141592653589793) + 1024 - self.offset)
+
         self.set_position_raw(position_raw, speed)
 
     def set_position_raw(self, position, speed=1000):
-        if position < 0:
-            position = -32768 - position
+        if self.id == 1:
+            if position < 0:
+                position = -32768 - position
+        else:
+            if position < 0:
+                position = -1024 - position
+                print("ahahahhahahahaha nico seit chunt nie so wiit siiiike✊")
         self.packet_handler.WritePosEx(self.id, position, int(speed), 0)
 
     def change_mode(self, mode):
@@ -50,3 +65,4 @@ class Motor:
         if self.mode != "velocity":
             raise ValueError("Motor is not in velocity mode. Call change_mode('velocity') first.")
         self.packet_handler.WriteSpec(self.id, int(speed), 0)
+        
---
+++ Servo_UnitTest.py
@@ -0,0 +1,25 @@
+"""
+Manual test script for StepperController on COM7.
+Run from the same folder as stepper_controller.py.
+"""
+
+import sys
+import time
+sys.path.insert(0, "..")   # adjust if needed
+
+from stepper import StepperController
+
+
+stepper = StepperController(offset1=0, offset2=0, com_port="COM7")
+home_status = stepper.home(2, speed=6000)
+print(f"Home status: {home_status}")
+home_status = stepper.home(1, speed=400)
+print(f"Home status: {home_status}")
+pos2 = stepper.get_position(2)
+print(f"Position of motor 2: {pos2:.2f} mm")
+status = stepper.set_position(2, -50, speed=4000)
+print(f"Set position status: {status}")
+time.sleep(20)
+pos3 = stepper.get_position(2)
+print(f"Position of motor 2 after move: {pos3:.2f} mm")
+
